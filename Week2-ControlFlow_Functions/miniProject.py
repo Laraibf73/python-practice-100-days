83cc4e9e4f6c6f8676351f34f2f6beb8672d5a07
laraibf73 <laraibf73@gmail.com>
2026-07-04 12:48:53 +0500

    mini project

--- Week2-ControlFlow_Functions/miniProject.py
+++ Week2-ControlFlow_Functions/miniProject.py
@@ -8,13 +8,17 @@
 '''
 
 balance = 1000
-
+deposit_history = []
+withdrawal_history = []
 def deposit():
     global balance
+    global deposit_history
     try:
         amount = float(input("Enter the amount to deposit: "))
         if amount > 0:
             balance += amount
+            deposit_amount=amount
+            deposit_history.append(deposit_amount)
             print(f"Successfully deposited ${amount:.2f}.\nNew balance: ${balance:.2f}")
         else:
             print("Invalid deposit amount. Please enter a positive value.")
@@ -25,12 +29,15 @@ def deposit():
 
 def withdraw():
     global balance
+    global withdrawal_history
     try:
         amount = float(input("Enter the amount to withdraw: "))
         if amount <= 0:
             print("Invalid withdrawal amount. Please enter a positive value.")
         elif amount <= balance:
             balance -= amount
+            withdrawal_amount=amount
+            withdrawal_history.append(withdrawal_amount)
             print(f"Successfully withdrew ${amount:.2f}.\nNew balance: ${balance:.2f}")
         else:
             print("Insufficient Balance!")
@@ -47,28 +54,44 @@ def display_menu():
     print("1. Check Balance")
     print("2. Deposit Money")
     print("3. Withdraw Money")
-    print("4. Exit")
+    print("4. Transaction History")
+    print("5. Exit")
 
+
+def transaction_history():
+    print("Transaction History:")
+    if not deposit_history and not withdrawal_history:
+        print("No transactions yet.")
+        return
+    print("Deposits:")
+    for deposit in deposit_history:
+        print(f"+${deposit:.2f} Deposited")
+    print("Withdrawals:")
+    for withdrawal in withdrawal_history:
+        print(f"-${withdrawal:.2f} Withdrawn")  
+        
 
 while True:
     display_menu()
     try:
-        choice = int(input("Enter your choice (1-4): "))
+        choice = int(input("Enter your choice (1-5): "))
     except ValueError:
-        print("Invalid input! Please enter a number between 1 and 4.")
+        print("Invalid input! Please enter a number between 1 and 5.")
         continue
 
     if choice == 1:
-        check_balance()
+       check_balance()
     elif choice == 2:
         deposit()
     elif choice == 3:
         withdraw()
     elif choice == 4:
+        transaction_history()
+    elif choice == 5:
         print("Thank you for using the Banking System. Goodbye!")
         break
     else:
-        print("Invalid input! Please enter a number between 1 and 4.")
+        print("Invalid input! Please enter a number between 1 and 5.")
 
 
 
